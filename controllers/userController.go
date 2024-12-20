67e1a21245e18b7bffca342b28d9607cc3f4e0a7
divineod <divineod9@gmail.com>
2024-12-20 13:03:34 +0100

    Included handlers for sign in with google

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,7 +2,11 @@ package controllers
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"log"
 	"net/http"
@@ -15,15 +19,149 @@ import (
 	helper "github.com/Adventure-Inc/users-backend/helpers"
 	model "github.com/Adventure-Inc/users-backend/models"
 
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+// Credentials which stores google ids.
+type Credentials struct {
+	Cid     string `json:"cid"`
+	Csecret string `json:"csecret"`
+}
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 var validate = validator.New()
+var cred Credentials
+var conf *oauth2.Config
+var state string
+
+// // var store
+
+// var (
+// 	googleOauthConfig = &oauth2.Config{
+// 		ClientID:     "530410014475-jg9o39oj6hcb1gsnne7c5hcg4jf54g2l.apps.googleusercontent.com",
+// 		ClientSecret: "530410014475-jg9o39oj6hcb1gsnne7c5hcg4jf54g2l.apps.googleusercontent.com",
+// 		RedirectURL:  "http://127.0.0.1:8000/auth/google/callback",
+// 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+// 		Endpoint:     google.Endpoint,
+// 	}
+// 	// Random state string to protect against CSRF attacks
+// 	oauthStateString = randToken()
+// )
+
+func randToken() string {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		log.Panic("Failed to generate random bytes for auth token")
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func init() {
+	file, err := os.ReadFile("../secrets/creds.json")
+	if err != nil {
+		log.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	json.Unmarshal(file, &cred)
+
+	conf = &oauth2.Config{
+		ClientID:     cred.Cid,
+		ClientSecret: cred.Csecret,
+		RedirectURL:  "http://127.0.0.1:9090/auth",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email", // You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
+		},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func getLoginURL(state string) string {
+	return conf.AuthCodeURL(state)
+}
+
+func GoogleLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		url := conf.AuthCodeURL(state, oauth2.AccessTypeOffline)
+		c.Redirect(http.StatusTemporaryRedirect, url)
+	}
+}
+
+func GoogleCallback() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		session := sessions.Default(c)
+		retrievedState := session.Get("state")
+
+		if retrievedState != c.Query("state") {
+			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+			return
+		}
+
+		code := c.Query("code")
+		if code == "" {
+			log.Println("No authorization code provided")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "No authorization code provided"})
+			return
+		}
+
+		// Exchange the authorization code for an access token
+		token, err := conf.Exchange(context.Background(), code)
+		if err != nil {
+			log.Println("Error while exchanging code for token:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to exchange code for token"})
+			return
+		}
+
+		// Retrieve user information from Google
+		client := conf.Client(context.Background(), token)
+
+		resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+
+		if err != nil {
+			log.Println("Failed to retrieve user information:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve user information"})
+			return
+		}
+		defer resp.Body.Close()
+
+		var userInfo map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+			log.Println("Failed to decode user information:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode user information"})
+			return
+		}
+
+		email := userInfo["email"].(string)
+		name := userInfo["name"].(string)
+
+		// Optionally, store or create a user record in the database
+		log.Println("User info:", userInfo)
+
+		// Respond with a success message or redirect to a protected page
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Login successful",
+			"name":    name,
+			"email":   email,
+		})
+	}
+}
+
+func loginHandler(c *gin.Context) {
+	state = randToken()
+	session := sessions.Default(c)
+	session.Set("state", state)
+	session.Save()
+	c.Writer.Write([]byte("<html><title>Golang Google</title> <body> <a href='" + getLoginURL(state) + "'><button>Login with Google!</button> </a> </body></html>"))
+}
 
 func GetUsers() gin.HandlerFunc {
 
